255c82fe5dc30aed973b402173f1eaf584c37cb9
euforic <cs@euforic.io>
2025-10-16 22:34:18 -0700

    Update wikimd to v0.1.0 with new checksums after force push

--- Formula/wikimd.rb
+++ Formula/wikimd.rb
@@ -11,20 +11,20 @@ class Wikimd < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/euforicio/wikimd/releases/download/v#{version}/wikimd_#{version}_Darwin_arm64.tar.gz"
-      sha256 "dd832269666a8011b01d3d360ed50f3a5fd9985d90e3d974a4506b0d66832d23"
+      sha256 "782fbf436bf42ccf55064f3fd57cc45dfa5c5b5bd1b6065165d17554f88edb8b"
     else
       url "https://github.com/euforicio/wikimd/releases/download/v#{version}/wikimd_#{version}_Darwin_x86_64.tar.gz"
-      sha256 "fc1b96769e17153cb308fa7da38a3502a67fefac578f133b3caf9211319ca67a"
+      sha256 "c8380e12fdf65343a39bc4f4b2c64ded41713611921c51ccdac2e0749ae78c52"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/euforicio/wikimd/releases/download/v#{version}/wikimd_#{version}_Linux_arm64.tar.gz"
-      sha256 "1dbfeb3c5aeab0cdbfd98dbc89a299b76ccbd03b35a8f5c2fa79fa3ff2d48f22"
+      sha256 "3bc30048f86a85a075e8c46bbb9f52b3547366eb635a6b4a3e88744344432337"
     else
       url "https://github.com/euforicio/wikimd/releases/download/v#{version}/wikimd_#{version}_Linux_x86_64.tar.gz"
-      sha256 "35451c4d61b5ad5df2f1c16a6f97de309535711bca806c3466637f489299fa66"
+      sha256 "02c86ddab0c7aedb7b3f55e2984cc01cde9014d0b1695b23816cffc39312a793"
     end
   end
 

27b1386342de4550590850d44d9af264865ccfcc
euforic <cs@euforic.io>
2026-01-29 17:59:56 -0800

    Update wikimd to v0.2.1

--- Formula/wikimd.rb
+++ Formula/wikimd.rb
@@ -1,7 +1,7 @@
 class Wikimd < Formula
   desc "Local-first Markdown wiki with live file watching and elegant UI"
   homepage "https://github.com/euforicio/wikimd"
-  version "0.2.0" # UPDATE THIS with each release
+  version "0.2.1" # UPDATE THIS with each release
   license "MIT"
 
   depends_on "ripgrep"
@@ -13,17 +13,17 @@ class Wikimd < Formula
       sha256 "8ddd8fe60efcf7185163bdc2cd25a65e4800efacd2ce9ce027b108728f19b396"
     else
       url "https://github.com/euforicio/wikimd/releases/download/v#{version}/wikimd_#{version}_Darwin_x86_64.tar.gz"
-      sha256 "8ed44ccea67d491eb6e2dab9a200b4676ba846fc6c01ae7bccf44643e8591311"
+      sha256 "36864c81f55fa7eaa65c3b4c0c469cb8cd145cd3ac3e6180156be705f3a336ad"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/euforicio/wikimd/releases/download/v#{version}/wikimd_#{version}_Linux_arm64.tar.gz"
-      sha256 "ea6e6027b7029b8b19dd0ccabce9ba19d37999e81e4911c43f52f3fd7cccfff9"
+      sha256 "2eab576028540ddbcd27bdbb5bf4416a03ee460b88945217a3f3e10d57f85d27"
     else
       url "https://github.com/euforicio/wikimd/releases/download/v#{version}/wikimd_#{version}_Linux_x86_64.tar.gz"
-      sha256 "b2e46c63467c80e5ba41d2fd6babc849153c1350ef1e74135ef3b9a5f7212e9a"
+      sha256 "a8b22c307321f471892bf9c240258e28af3a34c0b48f2fab88a243b08db203b7"
     end
   end
 

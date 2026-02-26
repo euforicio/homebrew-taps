e8f0655b283c171a9191753754b6f2dbac1da284
euforic <cs@euforic.io>
2026-02-25 21:53:21 -0800

    Pin scratchpad cask back to v0.1.0

--- Casks/scratchpad.rb
+++ Casks/scratchpad.rb
@@ -1,5 +1,5 @@
 cask "scratchpad" do
-  version "1.9.6"
+  version "0.1.0"
   sha256 "e7f7ae94c3216bb3a7be62152c3b10251396271ccc28419fe36e20afbece01f4"
 
   url "https://github.com/euforicio/scratchpad/releases/download/v#{version}/Scratchpad-#{version}.dmg"
